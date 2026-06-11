5dac5dcab41da923c9733795dda1df5054918a8f
delightlife <chetan.palwal@gmail.com>
2026-06-12 00:47:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach(question => {
+
+question.addEventListener("click", () => {
+
+const answer = question.nextElementSibling;
+
+if (answer.style.display === "block") {
+answer.style.display = "none";
+} else {
+
+document.querySelectorAll(".faq-answer").forEach(item => {
+item.style.display = "none";
+});
+
+answer.style.display = "block";
+}
+
+});
+
+});
+
+});
